8b1110b7f7155d792297695b539a06f40017a0f3
Vitalii_Zlotnyk <vitalikzlot12@gmail.com>
2026-09-17 19:37:14 +0200

    fix: update vite config for github pages and global variable

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,9 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-js-hw-11/', // Вказуємо базовий шлях для GitHub Pages
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      global: 'window', // Виправляємо помилку SimpleLightbox у продакшені
     },
     root: 'src',
     build: {
